fa54d42dba352f36a8ce46c30663eb2f2569075f
El Diablo <estesquefawer@gmail.com>
2020-05-30 10:04:00 -0500

    deploy to heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,12 +6,54 @@
 // ===============================================================================
 
 var tableArray = [
-    {
-      customerName: "Ahmed",
-      customerEmail: "ahmed@example.com",
-      customerID: "afhaque89",
-      phoneNumber: "000-000-0000"
-    }
+  {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"juan",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        1,
+        5,
+        3,
+        5,
+        4,
+        5
+      ]
+  },
+  {
+    "name":"Tom",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        1,
+        4,
+        1,
+        5,
+        1,
+        1,
+        5,
+        1,
+        1
+      ]
+  }
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
