cc2a3df8b31b82aecd5fe3457ac7c53b8f9b810a
El Diablo <estesquefawer@gmail.com>
2020-08-11 14:03:38 -0500

    modified routes and gave fucntionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,6 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
-
-var tableArray = [
+var newFriend = [
   {
-    "name":"Dmea",
+    "name":"Lupe",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         5,
@@ -23,7 +16,7 @@ var tableArray = [
       ]
   },
   {
-    "name":"juan",
+    "name":"Juan",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         5,
@@ -39,7 +32,7 @@ var tableArray = [
       ]
   },
   {
-    "name":"Tom",
+    "name":"Bob",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         1,
@@ -57,5 +50,5 @@ var tableArray = [
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = tableArray;
+  module.exports = newFriend;
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,75 +1,40 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+var friends = require("../data/friends");
 
-var tableArray = require("../data/friends");
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
 module.exports = function (app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function (req, res) {
-    res.json(tableArray);
+    res.json(friends);
   });
 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function (req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    var newFriend = req.body;
-    var newFriendScore= 0;
-    var existingFriendScore = 0;
-    var totalDifferences = 0;
-    var match = 1000;
-    var matchInfo = {};
+   var newScores = req.res.scores;
+   var newFriend = '';
+   var newFriendImg = '';
+   var totalDifference = 50;
 
-    for (let i = 0; i < tableArray.length; i++) {
-      for (let j = 0; j < tableArray[i].scores.length; j++) {
-        // add in totalDifference Math.abs('exisiting friend score ' -  'new friend score')
-        newFriendScore += Number(newFriend.enteredData[i]);
-        existingFriendScore += Number(tableArray[i].enteredData[i]);
+   for (var i = 0; i < friends.length; i++) {
+    var difference = 0;
 
-        if (Math.abs(newFriendScore - existingFriendScore) < match) {
-          match = Math.abs(newFriendScore - existingFriendScore);
-          matchInfo = tableArray[i];
-          console.log(matchData);
-      }
-       
-        //condition for resetting totalDifference and setting match
-      }
-      matchData.push(req.body);
-    }
+    // Compare scores
+    for (var j = 0; j < newScores.length; j++) {
+        difference += (Math.abs(parseInt(friends[i].scores[j])) - parseInt((newScores[j])));
+    };
 
-    //send match back to client
-    res.json(matchInfo);
-  });
+    if (difference < totalDifference) {
+        newFriend = friends[i].name;
+        newFriendImg = friends[i].photo;
+        totalDifference = difference;
+    };
+};
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+// console.log("Total difference: " + diff)
 
-  app.post("/api/clear", function (req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
-  });
+friends.push(req.body); // Add new friend to friends array
+res.json({ status: 'OK', name: newFriend, photo: newFriendImg }); // Send back response to browser the best friend match
+console.log("Your new best friend is: " + newFriend, newFriendImg);
+});
 };
